d2d2d1d89d26fc358960f143d9ff90d1a011564d
rguixaro <developer@rguixaro.dev>
2025-11-08 19:54:18 +0100

    feat(miscellaneous): add utils

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+/**
+ * Icon properties
+ * @type {Object}
+ */
+export const IconProps = { color: '#525252', size: 18 }
+
+/**
+ * Site URL
+ * @type {string}
+ */
+export const SITE_URL = 'https://roots.rguixaro.dev'

cde29c369ee010e96ceda52e07a832b43bad806c
rguixaro <developer@rguixaro.dev>
2025-12-07 21:06:18 +0100

    feat(pages): add auth pages

--- src/app/trees/[slug]/page.tsx
+++ src/app/trees/[slug]/page.tsx
@@ -1,10 +1,9 @@
 import { getTranslations } from 'next-intl/server'
-import { Utensils } from 'lucide-react'
 
 import { auth } from '@/auth'
 import { getTreeRoots } from '@/server/queries'
 
-import { GoBack } from '@/components/layout'
+import { GoBack, NotFoundClient } from '@/components/layout'
 import { TreeWrapper } from '@/components/tree'
 
 import { TypographyH4 } from '@/ui'
@@ -17,18 +16,13 @@ export default async function TreePage({ params }: { params: Promise<{ slug: str
 
   const result = await getTreeRoots(slug)
   const t_errors = await getTranslations('errors')
+  const t_common = await getTranslations('common')
 
   if (!result) return null
 
   if ('error' in result) {
     return (
-      <div className="mt-2 flex flex-col pt-2 text-center">
-        <GoBack text={'trees'} className="px-8 md:px-10 lg:px-32 xl:px-52 2xl:px-64" />
-        <div className="text-ocean-200 mt-10 flex h-32 flex-col items-center justify-center">
-          <TypographyH4>{t_errors('error-tree-not-found')}</TypographyH4>
-          <Utensils size={24} className="mt-2 mb-5" />
-        </div>
-      </div>
+      <NotFoundClient pageNotFound={t_common('page-not-found')} returnText={t_common('return')} />
     )
   }
 

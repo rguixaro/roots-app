b421682c1059176994fcfb469f12dd3a30087619
rguixaro <developer@rguixaro.dev>
2025-12-13 10:53:21 +0100

    feat(ui): update tree feed rendering

--- src/components/trees/tree/feed.tsx
+++ src/components/trees/tree/feed.tsx
@@ -14,9 +14,15 @@ export const TreesFeed = async () => {
 
   return (
     <div className="flex h-full w-full flex-col">
-      <p className="mt-2 mb-4">{t_common('trees-description')} </p>
-      <div className="no-scrollbar flex w-full justify-start overflow-x-auto">
-        <div className="flex w-max flex-row gap-1 p-2">
+      <div className="w-3/4 self-center sm:w-3/4">
+        <div className="text-ocean-400 flex h-full items-center justify-center">
+          <div className="h-full w-full sm:w-4/5 md:w-3/5">
+            <p className="text-ocean-400 mt-2 mb-4">{t_common('trees-description')}</p>
+          </div>
+        </div>
+      </div>
+      <div className="no-scrollbar mx-auto w-full overflow-x-auto overflow-y-hidden sm:w-[90%] md:w-[75%] lg:w-[65%] xl:w-[55%]">
+        <div className="flex w-max flex-row gap-2 px-4">
           {treesWithAdd.map((item, i) => (
             <ItemTree key={i} tree={item} index={i} />
           ))}

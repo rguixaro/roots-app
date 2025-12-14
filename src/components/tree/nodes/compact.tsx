1858b412acc209e770799d6655be2a423735e371
rguixaro <developer@rguixaro.dev>
2025-12-14 20:21:57 +0100

    feat(pictures): optimize image with NextJS Image

--- src/components/tree/nodes/compact.tsx
+++ src/components/tree/nodes/compact.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { useState, useCallback, JSX, useEffect } from 'react'
+import Image from 'next/image'
 import { Position, Handle, NodeProps } from 'reactflow'
-import { Info, Image, LoaderIcon } from 'lucide-react'
+import { Info, Image as ImageIcon, LoaderIcon } from 'lucide-react'
 import { motion, Variants } from 'framer-motion'
 
 import { cn, getProfilePicture } from '@/utils'
@@ -312,19 +313,24 @@ export function StyledNodeCompact({ data }: NodeProps<SStyledNodeCompactProps>):
                 : 'pointer-events-none scale-95 opacity-0'
             )}
           >
-            <Image size={24} />
+            <ImageIcon size={24} />
           </div>
           {profilePicture && !pictureError && (
-            <img
+            <div
               className={cn(
-                'h-full w-full object-cover transition-all duration-300 ease-out',
+                'h-full w-full transition-all duration-300 ease-out',
                 isPictureLoading ? 'scale-95 opacity-0' : 'scale-100 opacity-100'
               )}
-              src={`${process.env.NEXT_PUBLIC_CLOUDFRONT_ASSETS_DOMAIN}/${profilePicture.fileKey}`}
-              alt="Profile"
-              onLoad={handlePictureLoad}
-              onError={handlePictureError}
-            />
+            >
+              <Image
+                src={`${process.env.NEXT_PUBLIC_CLOUDFRONT_ASSETS_DOMAIN}/${profilePicture.fileKey}`}
+                alt="Profile"
+                fill
+                style={{ objectFit: 'cover' }}
+                onLoadingComplete={handlePictureLoad}
+                onError={handlePictureError}
+              />
+            </div>
           )}
         </motion.div>
       </motion.div>

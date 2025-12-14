e65c39fcb771a63fbf7b67916ace3714758b6ebd
rguixaro <developer@rguixaro.dev>
2025-12-14 20:40:51 +0100

    feat(node): update node rendering on mobile devices

--- src/components/tree/nodes/compact.tsx
+++ src/components/tree/nodes/compact.tsx
@@ -244,9 +244,9 @@ export function StyledNodeCompact({ data }: NodeProps<SStyledNodeCompactProps>):
       'bg-ocean-100 h-8 w-3': isConnected,
       'border-ocean-100 bg-ocean-100 w-3': isHoveredOrExpanded,
       'border-ocean-100 bg-ocean-50 h-8': isHoveredAndNotConnected,
-      'border-ocean-100 bg-ocean-200 h-10': isExpanded,
-      'bg-ocean-300': isExpandedAndIsConnected,
-      'border-pale-ocean bg-ocean-200 h-8 w-3': isMobileAndNotConnected,
+      'border-ocean-100 bg-ocean-200 h-8': isExpanded,
+      'bg-ocean-300 h-12': isExpandedAndIsConnected,
+      'border-pale-ocean bg-ocean-50 h-8 w-3': isMobileAndNotConnected,
     })
   }
 
@@ -266,9 +266,9 @@ export function StyledNodeCompact({ data }: NodeProps<SStyledNodeCompactProps>):
       'bg-ocean-100 h-3 w-10': isConnected,
       'border-ocean-100 bg-ocean-200 h-3': isHoveredOrExpanded,
       'border-ocean-100 bg-ocean-50 h-3 w-10': isHoveredAndNotConnected,
-      'border-ocean-100 bg-ocean-200 h-3 w-20': isExpanded,
+      'border-ocean-100 bg-ocean-200 h-3 w-10': isExpanded,
       'bg-ocean-300 h-3 w-20': isExpandedAndIsConnected,
-      'border-pale-ocean bg-ocean-200 h-3 w-10': isMobileAndNotConnected,
+      'border-pale-ocean bg-ocean-50 h-3 w-10': isMobileAndNotConnected,
     })
   }
 
@@ -284,8 +284,8 @@ export function StyledNodeCompact({ data }: NodeProps<SStyledNodeCompactProps>):
       className={cn(
         'text-ocean-400 group relative flex h-20 w-auto cursor-pointer items-center justify-start rounded-lg',
         'shadow-center-sm hover:bg-ocean-100 hover:text-pale-ocean bg-pale-ocean cursor-pointer p-2 outline-none select-none focus:outline-none',
-        isExpanded && `bg-ocean-200 text-pale-ocean ${withPicture && 'rounded-b-none'}`,
-        isInModal && `bg-ocean-200 text-pale-ocean ${withPicture && 'rounded-b-none'}`
+        isExpanded && `bg-ocean-100 text-pale-ocean ${withPicture && 'rounded-b-none'}`,
+        isInModal && `bg-ocean-100 text-pale-ocean ${withPicture && 'rounded-b-none'}`
       )}
     >
       <motion.div variants={pictureContentVariants} className="px-2">
@@ -308,6 +308,7 @@ export function StyledNodeCompact({ data }: NodeProps<SStyledNodeCompactProps>):
           <div
             className={cn(
               'bg-ocean-50 group-hover:bg-ocean-100 absolute inset-0 flex items-center justify-center transition-all duration-300 ease-out',
+              isExpanded && 'bg-ocean-100',
               showPicturePlaceholder
                 ? 'scale-100 opacity-100'
                 : 'pointer-events-none scale-95 opacity-0'

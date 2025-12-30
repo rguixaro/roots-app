a9244c034cbd317a067054444166afc453b4b940
rguixaro <developer@rguixaro.dev>
2025-12-30 20:23:36 +0100

    feat(server): minor tweaks

--- src/server/queries/index.ts
+++ src/server/queries/index.ts
@@ -66,7 +66,7 @@ export const getTreeRoots = cache(async (slug: string) => {
     const nodes = await db.treeNode.findMany({
       where: { treeId: tree.id },
       include: { taggedIn: { where: { isProfile: true }, include: { picture: true } } },
-      orderBy: [{ birthDate: 'asc' }, { createdAt: 'asc' }],
+      orderBy: [{ birthDate: 'asc' }, { createdAt: 'desc' }],
     })
 
     const typedNodes = nodes.map((node) => ({

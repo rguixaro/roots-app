35b4cbc719186b9e72abee7c4a813b2463a4762d
rguixaro <developer@rguixaro.dev>
2026-01-03 16:34:33 +0100

    feat(config): add aws ses package and update env credentials

--- src/env.mjs
+++ src/env.mjs
@@ -33,6 +33,7 @@ export const env = createEnv({
     AMAZON_S3_BUCKET_NAME: z.string(),
     AMAZON_CLOUDFRONT_KEY_PAIR_ID: z.string(),
     AMAZON_CLOUDFRONT_PRIVATE_KEY_SECRET_NAME: z.string(),
+    AMAZON_SES_FROM_EMAIL: z.string().email(),
 
     /// Public
     NEXT_PUBLIC_CLOUDFRONT_ASSETS_DOMAIN: z.string().url(),
@@ -66,6 +67,7 @@ export const env = createEnv({
     AMAZON_CLOUDFRONT_KEY_PAIR_ID: process.env.AMAZON_CLOUDFRONT_KEY_PAIR_ID,
     AMAZON_CLOUDFRONT_PRIVATE_KEY_SECRET_NAME:
       process.env.AMAZON_CLOUDFRONT_PRIVATE_KEY_SECRET_NAME,
+    AMAZON_SES_FROM_EMAIL: process.env.AMAZON_SES_FROM_EMAIL,
 
     /// Public
     NEXT_PUBLIC_CLOUDFRONT_ASSETS_DOMAIN: process.env.NEXT_PUBLIC_CLOUDFRONT_ASSETS_DOMAIN,
